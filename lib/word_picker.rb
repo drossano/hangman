418608c40379bc2e1fd369c82a0f755e45c4d347
Dean <72749793+drossano@users.noreply.github.com>
2022-01-15 14:53:54 -0500

    Pick random word between 5 and 12 letters from dictionary

--- lib/word_picker.rb
+++ lib/word_picker.rb
@@ -1,8 +1,18 @@
 class WordPicker
+  attr_reader :word
   def initialize
-    IO.sysopen 'dictionary.txt'
-    @dictionary = IO.new(5)
+    @dictionary = IO.new(IO.sysopen 'dictionary.txt')
+    @word_list = Array.new
+    @word = create_word_list.sample
+  end
+
+  def create_word_list
+      until @dictionary.eof? == true do
+        word = @dictionary.gets.chomp
+        if word.length >= 5 && word.length <= 12
+          @word_list.push word
+        end
+      end
+      @word_list
   end
 end
-
-WordPicker.new
