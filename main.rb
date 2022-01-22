814a3b6455ed2ff851053a63fb21aeb2e459dc23
Dean <72749793+drossano@users.noreply.github.com>
2022-01-22 10:46:36 -0500

    Add input validation loop to loading game prompt

--- main.rb
+++ main.rb
@@ -7,12 +7,18 @@ serialized_file = "./saved_games/saved_game.yaml"
 if File.exist?(serialized_file)
   puts "A saved game is detected. Would you like to resume it? (Enter \"yes\" or \"no\")"
   response = gets.chomp.downcase
-  if response == "yes"
-    p saved_game = load_game
-    saved_game.play_game
-    File.delete(serialized_file)
-  elsif response == "no"
-    Game.new.play_game
+  loop do
+    if response == "yes"
+      p saved_game = load_game
+      saved_game.play_game
+      File.delete(serialized_file)
+      break
+    elsif response == "no"
+      Game.new.play_game
+    else
+      puts "Invaled choice. Enter \"yes\" or \"no\" if you would like to resume your saved game."
+      response = gets.chomp.downcase
+    end
   end
 else
   Game.new.play_game
