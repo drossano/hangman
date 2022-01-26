c781e11e5ef28fb11145ad8b3da8a168b694a7c7
Dean <72749793+drossano@users.noreply.github.com>
2022-01-25 20:24:15 -0500

    Extract input validation from overwrite_check

--- lib/game.rb
+++ lib/game.rb
@@ -51,21 +51,24 @@ class Game
   def overwrite_check
     if File.exist?("./saved_games/saved_game.yaml")
       puts "A saved game already exists. Would you like to overwrite it? (Enter \"yes\" or \"no\")"
-      response = gets.chomp.downcase
-      loop do
-        if response == "yes"
-          save_game(self)
-          break
-        elsif response == "no"
-          play_game
-          break
-        else
-          puts "Invaled choice. Enter \"yes\" or \"no\" if you would like to overwrite your saved game."
-          response = gets.chomp.downcase
-        end
-      end
+      validate_overwrite_response(gets.chomp.downcase)
     else
       save_game(self)
+    end
+  end
+
+  def validate_overwrite_response(response)
+    loop do
+      if response == "yes"
+        save_game(self)
+        break
+      elsif response == "no"
+        play_game
+        break
+      else
+        puts "Invaled choice. Enter \"yes\" or \"no\" if you would like to overwrite your saved game."
+        response = gets.chomp.downcase
+      end
     end
   end
 
