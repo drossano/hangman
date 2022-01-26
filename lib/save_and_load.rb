521654893e8832d871078fde3d085cd2731a824c
Dean <72749793+drossano@users.noreply.github.com>
2022-01-25 20:19:53 -0500

    Move overwrite_check back to game.rb

--- lib/save_and_load.rb
+++ lib/save_and_load.rb
@@ -15,25 +15,4 @@ module SaveAndLoad
     puts "Loading game..."
     YAML::load(yaml)
   end
-
-  def overwrite_check
-    if File.exist?("./saved_games/saved_game.yaml")
-      puts "A saved game already exists. Would you like to overwrite it? (Enter \"yes\" or \"no\")"
-      response = gets.chomp.downcase
-      loop do
-        if response == "yes"
-          save_game(self)
-          break
-        elsif response == "no"
-          play_game
-          break
-        else
-          puts "Invaled choice. Enter \"yes\" or \"no\" if you would like to overwrite your saved game."
-          response = gets.chomp.downcase
-        end
-      end
-    else
-      save_game(self)
-    end
-  end
 end
